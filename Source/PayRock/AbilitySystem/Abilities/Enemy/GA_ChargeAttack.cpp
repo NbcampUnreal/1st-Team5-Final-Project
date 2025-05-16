f94755ac9ddf9f02809177de376f6c5f0b4ab26a
Midam <121799933+GolGol00@users.noreply.github.com>
2025-05-16 16:23:07 +0900

    [Fix] 장산범 BT 고장나는거 해결 및 어택 완료

--- Source/PayRock/AbilitySystem/Abilities/Enemy/GA_ChargeAttack.cpp
+++ Source/PayRock/AbilitySystem/Abilities/Enemy/GA_ChargeAttack.cpp
@@ -17,8 +17,9 @@ void UGA_ChargeAttack::ActivateAbility(
 	const FGameplayAbilityActivationInfo ActivationInfo,
 	const FGameplayEventData* TriggerEventData)
 {
-
 	
+	Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
+
 	
 	if (!CommitAbility(Handle, ActorInfo, ActivationInfo)) return;
 
@@ -30,18 +31,18 @@ void UGA_ChargeAttack::ActivateAbility(
 		if (UBlackboardComponent* BB = AICon->GetBlackboardComponent())
 		{
 			BB->SetValueAsBool("bIsAttacking", true);
-			Tiger->PlayAnimMontage(Tiger->GetChargingMontage());
+			//Tiger->PlayAnimMontage(Tiger->GetChargingMontage());
 			AICon->StopMovement();
 		}
 	}
 	
-	Tiger->GetWorldTimerManager().SetTimer(
-		TelegraphTimerHandle,
-		this,
-		&UGA_ChargeAttack::OnChargeStart,
-		TelegraphDuration,
-		false
-	);
+	// Tiger->GetWorldTimerManager().SetTimer(
+	// 	TelegraphTimerHandle,
+	// 	this,
+	// 	&UGA_ChargeAttack::OnChargeStart,
+	// 	TelegraphDuration,
+	// 	false
+	// );
 }
 
 void UGA_ChargeAttack::OnChargeStart()
@@ -49,7 +50,7 @@ void UGA_ChargeAttack::OnChargeStart()
 	if (!Tiger) return;
 
 	
-	Tiger->PlayAnimMontage(Tiger->GetRushAttackMontage());
+	//Tiger->PlayAnimMontage(Tiger->GetRushAttackMontage());
 	const FVector Forward = Tiger->GetActorForwardVector();
 	Tiger->LaunchCharacter(Forward * ChargeSpeed, true, true);
 
@@ -59,13 +60,13 @@ void UGA_ChargeAttack::OnChargeStart()
 		Tiger->ChargeCollision->OnComponentBeginOverlap.AddDynamic(this, &UGA_ChargeAttack::OnChargeHit);
 	}
 
-	Tiger->GetWorldTimerManager().SetTimer(
-		EndChargeTimerHandle,
-		this,
-		&UGA_ChargeAttack::EndCharge,
-		ChargeDuration,
-		false
-	);
+	// Tiger->GetWorldTimerManager().SetTimer(
+	// 	EndChargeTimerHandle,
+	// 	this,
+	// 	&UGA_ChargeAttack::EndCharge,
+	// 	ChargeDuration,
+	// 	false
+	// );
 }
 
 void UGA_ChargeAttack::OnChargeHit(
@@ -86,7 +87,6 @@ void UGA_ChargeAttack::EndCharge()
 {
 	if (!Tiger) return;
 
-	Tiger->GetCharacterMovement()->StopMovementImmediately();
 
 	if (Tiger->ChargeCollision)
 	{
@@ -94,7 +94,6 @@ void UGA_ChargeAttack::EndCharge()
 		Tiger->ChargeCollision->OnComponentBeginOverlap.RemoveAll(this);
 	}
 
-	// bIsAttacking 초기화
 	if (AAIController* AICon = Cast<AAIController>(Tiger->GetController()))
 	{
 		if (UBlackboardComponent* BB = AICon->GetBlackboardComponent())
@@ -104,5 +103,5 @@ void UGA_ChargeAttack::EndCharge()
 		}
 	}
 
-	EndAbility(CurrentSpecHandle, CurrentActorInfo, CurrentActivationInfo, true, false);
+	//EndAbility(CurrentSpecHandle, CurrentActorInfo, CurrentActivationInfo, true, false);
 }
