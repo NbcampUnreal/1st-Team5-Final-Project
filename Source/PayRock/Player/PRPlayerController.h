a353114002cd96c3d47f70300daa35e1641aae8c
nonecalculate <escortbayonet@gmail.com>
2025-05-19 12:24:10 +0900

    Add TestLevel, Fix Spector
    관전할때 사망한 플레이어 엑세스 하는 오류와 서버는 시야를 따라가는데 클라이언트는 못따라가는 버그, 게임모드 픽스 및 패키징용 테스트레벨을 추가하였습니다.

--- Source/PayRock/Player/PRPlayerController.h
+++ Source/PayRock/Player/PRPlayerController.h
@@ -73,14 +73,19 @@ public:
 
 	FString GetNetModeAsString() const; // 디버깅용 함수
 	FShowLeaveOptionsDelegate ShowLeaveOptionsDelegate;
+
+	UFUNCTION(Client, Reliable)
+	void Client_OnSpectateTargetDied(AActor* DeadActor);
 	
+	void OnSpectateTargetDied(AActor* DeadActor); // 관전 타겟 사망시 그 타겟 제거
 protected:
 	virtual void BeginPlay() override;
 
-	void BindingSpector();
+	void BindingSpector(); // 스펙터
 private:
-	void SetSpectateTarget(AActor* NewTarget);
+	void SetSpectateTarget(AActor* NewTarget); // 관전 타겟 설정
 
+	
 	UPROPERTY()
 	TArray<AActor*> SpectateTargets;
 
