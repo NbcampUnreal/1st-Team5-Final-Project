52ff9b1eb5d5ed31bb2ba46cebf1e551c39a8d2b
kimjuyeong <kimjy2767@naver.com>
2025-05-19 16:27:13 +0900

    FIx] 갓파공격, 각 BT들 Hit때 멈춤2

--- Source/PayRock/AbilitySystem/Abilities/Enemy/GA_KappaAttack.h
+++ Source/PayRock/AbilitySystem/Abilities/Enemy/GA_KappaAttack.h
@@ -13,4 +13,23 @@ UCLASS()
 class PAYROCK_API UGA_KappaAttack : public UBaseDamageGameplayAbility
 {
 	GENERATED_BODY()
+public:
+	virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData) override;
+	virtual void EndAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, bool bReplicateEndAbility, bool bWasCancelled) override;
+	
+protected:
+	UFUNCTION(BlueprintCallable)
+	void BindCallbackToFistCollision();
+	
+	UFUNCTION(BlueprintCallable)
+	void ToggleFistCollision(bool bShouldEnable);
+
+	UFUNCTION()
+	void OnFistOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp,
+		int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	UPROPERTY(BlueprintReadOnly)
+	TObjectPtr<class AKappaMonster> AvatarCharacter;
+
+	bool bHit = false;
 };
--- Source/PayRock/Enemy/BTTesk/Kappa/BTTask_ActivateDetectAbility.cpp
+++ Source/PayRock/Enemy/BTTesk/Kappa/BTTask_ActivateDetectAbility.cpp
@@ -1,4 +1,32 @@
-﻿// PayRockGames
-
-
+﻿
 #include "BTTask_ActivateDetectAbility.h"
+#include "AbilitySystemComponent.h"
+#include "AIController.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "PayRock/Enemy/EnemyCharacter.h"
+
+UBTTask_ActivateDetectAbility::UBTTask_ActivateDetectAbility()
+{
+	NodeName = "Kappa Activate Detect Ability";
+}
+
+EBTNodeResult::Type UBTTask_ActivateDetectAbility::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+	AAIController* AICon = OwnerComp.GetAIOwner();
+	AEnemyCharacter* Enemy = Cast<AEnemyCharacter>(AICon ? AICon->GetPawn() : nullptr);
+	if (!Enemy || !DetectAbility) return EBTNodeResult::Failed;
+
+	UAbilitySystemComponent* ASC = Enemy->GetAbilitySystemComponent();
+	if (!ASC) return EBTNodeResult::Failed;
+
+	if (ASC->TryActivateAbilityByClass(DetectAbility))
+	{
+		if (UBlackboardComponent* BB = OwnerComp.GetBlackboardComponent())
+		{
+			BB->SetValueAsBool(DetectKey, true);
+		}
+		return EBTNodeResult::Succeeded;
+	}
+
+	return EBTNodeResult::Failed;
+}
