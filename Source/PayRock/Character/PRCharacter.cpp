e59be2bf61ffbb17ecaf0488be76c163b05b2e22
CseJune <tpwns9541@gmail.com>
2025-06-10 16:41:41 +0900

    캐릭터 속도 조정 및 카메라 조정

--- Source/PayRock/Character/PRCharacter.cpp
+++ Source/PayRock/Character/PRCharacter.cpp
@@ -58,7 +58,7 @@ APRCharacter::APRCharacter()
     Weapon2->SetCollisionEnabled(ECollisionEnabled::NoCollision);
     BlessingComponent = CreateDefaultSubobject<UBlessingComponent>(TEXT("BlessingComponent"));
 
-    NormalSpeed = 350.0f;
+    NormalSpeed = 300.0f;
     SprintSpeedMultiplier = 2.0f;
     CrouchSpeed = 200.0f;
     SprintSpeed = NormalSpeed * SprintSpeedMultiplier;
