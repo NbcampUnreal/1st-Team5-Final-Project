7b02298117561564aa395e6e3edc85cdcde46ed4
jeongukchoi <jwchoi955@gmail.com>
2025-06-25 19:09:21 +0900

    JC-도깨비 무기 작업 중

---
+++ Source/PayRock/AbilitySystem/Abilities/Weapons/DoggebiWeaponSkill.cpp
@@ -0,0 +1,13 @@
+// PayRockGames
+
+
+#include "DoggebiWeaponSkill.h"
+
+void UDoggebiWeaponSkill::ActivateAbility(const FGameplayAbilitySpecHandle Handle,
+	const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo,
+	const FGameplayEventData* TriggerEventData)
+{
+	Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
+
+	
+}
---
+++ Source/PayRock/AbilitySystem/Abilities/Weapons/DoggebiWeaponSkill.h
@@ -0,0 +1,17 @@
+// PayRockGames
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PayRock/AbilitySystem/Abilities/BaseDamageGameplayAbility.h"
+#include "DoggebiWeaponSkill.generated.h"
+
+
+UCLASS()
+class PAYROCK_API UDoggebiWeaponSkill : public UBaseDamageGameplayAbility
+{
+	GENERATED_BODY()
+
+public:
+	virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData) override;
+};
