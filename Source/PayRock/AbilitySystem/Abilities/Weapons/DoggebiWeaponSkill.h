178c2e168ddc8dd812f1ad2a8fe8ce3d0df5ac57
jeongukchoi <jwchoi955@gmail.com>
2025-06-26 10:03:58 +0900

    JC-도깨비 스킬 작업중

--- Source/PayRock/AbilitySystem/Abilities/Weapons/DoggebiWeaponSkill.h
+++ Source/PayRock/AbilitySystem/Abilities/Weapons/DoggebiWeaponSkill.h
@@ -4,14 +4,34 @@
 
 #include "CoreMinimal.h"
 #include "PayRock/AbilitySystem/Abilities/BaseDamageGameplayAbility.h"
+#include "PayRock/AbilitySystem/Abilities/BaseMontageDamageSkill.h"
 #include "DoggebiWeaponSkill.generated.h"
 
 
 UCLASS()
-class PAYROCK_API UDoggebiWeaponSkill : public UBaseDamageGameplayAbility
+class PAYROCK_API UDoggebiWeaponSkill : public UBaseMontageDamageSkill
 {
 	GENERATED_BODY()
 
 public:
-	virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData) override;
+	
+	virtual void OnEventReceived(FGameplayEventData Payload) override;
+	virtual void EndAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, bool bReplicateEndAbility, bool bWasCancelled) override;
+
+protected:
+	UFUNCTION()
+	void TriggerSpinAttack(FGameplayTag Tag, int Count);
+	bool SpawnDoggebiSpinActor();
+
+	UPROPERTY(EditDefaultsOnly)
+	float Duration;
+	UPROPERTY(EditDefaultsOnly)
+	TSubclassOf<AActor> SpawnClass;
+	UPROPERTY(EditDefaultsOnly)
+	FVector SpawnOffset;
+	UPROPERTY()
+	AActor* SpawnedActor;
+	
+	
+	FTimerHandle StopTimer;
 };
